af95d5b91acfe44e0fc7155724682c28dd3c2399
zhongshuyi <zhongshuyi5214@foxmail.com>
2024-01-04 16:52:09 +0800

    ✨ feat: 添加自定义样式的 primevue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <RouterView />
+  <!-- <RouterView /> -->
+  <div class="mt-4 ml-4">
+    <Button label="Submit" severity="success" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import Button from 'primevue/button'
+</script>
 
 <style scoped></style>
