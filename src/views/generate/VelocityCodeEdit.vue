802583c9d2670f77388a6695ffb2e2f02559777e
zhongshuyi <zhongshuyi5214@foxmail.com>
2024-05-10 13:48:26 +0800

    ✨ feat: 整合代码编辑器组件

--- src/views/generate/VelocityCodeEdit.vue
+++ src/views/generate/VelocityCodeEdit.vue
@@ -1,9 +1,35 @@
 <template>
-  <div></div>
+  <Codemirror
+    v-model="codeString"
+    placeholder="Code here..."
+    :style="{ height: '400px' }"
+    :autofocus="true"
+    :indent-with-tab="true"
+    :tabSize="2"
+    :extensions="extensions"
+  />
 </template>
 
 <script setup lang="ts">
   defineOptions({ name: 'VelocityCodeEdit' })
+
+  import { Codemirror } from 'vue-codemirror'
+  import { defaultHighlightStyle, StreamLanguage, syntaxHighlighting } from '@codemirror/language'
+  import { velocity } from '@codemirror/legacy-modes/mode/velocity'
+
+  const props = defineProps({
+    code: {
+      type: String,
+      default: ''
+    }
+  })
+  const emit = defineEmits(['update:code'])
+  const codeString = useVModel(props, 'code', emit)
+
+  const extensions = [
+    StreamLanguage.define(velocity),
+    syntaxHighlighting(defaultHighlightStyle, { fallback: true })
+  ]
 </script>
 
 <style scoped lang="scss"></style>
--- src/views/generate/index.vue
+++ src/views/generate/index.vue
@@ -2,18 +2,20 @@
   <div class="w-9/10 m-a mt-10">
     <!-- <DictView :data="dict" />
     <EntityView :dict-data="dict" /> -->
-    <GlobalProperties :dict-data="dict" />
+    <!--    <GlobalProperties :dict-data="dict" />-->
+    <VelocityCodeEdit />
   </div>
 </template>
 <script lang="ts" setup>
   defineOptions({ name: 'GenerateView' })
   // import EntityView from '@/views/generate/Entity.vue'
   // import DictView from '@/views/generate/Dict.vue'
-  import { Dict } from './types'
-  import GlobalProperties from '@/views/generate/GlobalProperties.vue'
+  // import { Dict } from './types'
+  // import GlobalProperties from '@/views/generate/GlobalProperties.vue'
+  import VelocityCodeEdit from '@/views/generate/VelocityCodeEdit.vue'
 
-  const dict = ref<Dict[]>([
-    { dictName: 'MysqlType', dictValue: ['bigint', 'varchar'], inputValue: '' }
-  ])
+  // const dict = ref<Dict[]>([
+  //   { dictName: 'MysqlType', dictValue: ['bigint', 'varchar'], inputValue: '' }
+  // ])
 </script>
 <style lang="scss" scoped></style>
